be5ac9a4052002ccb5370f00237d71f4a486096a
Callum Colman <callumcolman@gmail.com>
2018-02-09 13:21:04 +0000

    First Attempt

--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -1,32 +1,28 @@
-<!DOCTYPE html>
-<html>
-    <head>
-         <title>Select Production</title>
-         
-         <!--jQuery-->
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-         <script type="text/javascript" src="cost_calc.js"></script>
-         
-    </head>
+/*global $ */
+
+function calcSub() {
+    var argSubTotal;
     
-    <body>
-           
-            <h4>Select a Product</h4>
-            
-            <br/>
-            
-                <form method="POST" action="Ebus2.php">
-                    
-                    <label for="salesforce">
-                        <input type="radio" id="salesforce" name="product" checked onCLcik="disablebtnProceed">
-                        SalesForce @ $100
-                    <button type="submit" id="btnPurchase" disabled>proceed with Purchase</button>
-                    
-                </form>
-                
-                <br/>
-                <button onClick="validateDetails()">Validate</button>
-                
-            <script type="text/javascript" src="ebus_validator.js"></script>
-    </body>
-</html>
+    if(document.getElementById('salesforce').checked) {
+        argSubTotal=100;
+    }
+    else{
+        argSubTotal=300;
+    }
+    display(argSubTotal);
+}
+
+function display(parm1) {
+    document.getElementById('subtotal').value = parm1;
+    document.getElementById('total').value =parm1;
+    
+    enablebtnProceed();
+}
+
+function enablebtnProceed(){
+    $('#btnProceed').prop('disabled', false);
+}
+
+function disablebtnProceed(){
+    $('#btnProceed').prop('disabled', true);
+}
