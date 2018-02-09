ec0be770d1dfc677f02815c65d73b7eec3a4628c
Callum Colman <callumcolman@gmail.com>
2018-02-09 12:49:53 +0000

    first commit

---
+++ ebusiness/Ebus2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title>Enter Details</title>
+         
+         <!--jQuery-->
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+            <h4>Please enter your payment details</h4>
+            
+                <form action="Ebus3.php" method="POST">
+                    
+                        <label for="user_pin">PIN</label>
+                        
+                        <input type="password" id="user_pin" placeholder="Card PIN" maxlength="4">
+                        
+                    <button type="submit" id="btnPurchase" disabled>proceed with Purchase</button>
+                    
+                </form>
+                
+                <br/>
+                <button onClick="validateDetails()">Validate</button>
+                
+            <script type="text/javascript" src="ebus_validator.js"></script>
+    </body>
+</html>
